f3f28d4b7b08cdc08c77f7fb3be722e79baf6c37
Colletta <45748736+colletta-official@users.noreply.github.com>
2018-12-12 16:14:14 +0900

    Update leaf-gen.js

--- js/leaf-gen.js
+++ js/leaf-gen.js
@@ -3,36 +3,67 @@ var bgSketch=function(d)
 	d.setup=function()
 	{
 		d.createCanvas(window.innerWidth,window.innerHeight);
-		d.background(255);
+		d.noLoop();
+		d.noStroke();
 	};
 	d.draw=function()
 	{
-    d.fill(0);
-		d.ellipse(50,50,50,50);
+		d.background(255);
+		var vmin=d.min(d.width,d.height);
+		var size=vmin/4;
+		var delta=vmin/12;
+		var hNo=d.round(d.width/40);
+		var vNo=d.round(d.height/40);
+		var i;
+		for(i=0;i<=hNo;i++)
+		{
+			d.leaf((i+0.5)*d.width/hNo,0,size+d.random(-delta,delta),d.PI/2+d.random(-0.5,0.5));
+			d.leaf((i+0.5)*d.width/hNo,d.height,size+d.random(-delta,delta),d.PI/2+d.random(-0.5,0.5));
+		}
+		for(i=0;i<=vNo;i++)
+		{
+			d.leaf(0,(i+0.5)*d.height/vNo,size+d.random(-delta,delta),d.random(-0.5,0.5));
+			d.leaf(d.width,(i+0.5)*d.height/vNo,size+d.random(-delta,delta),d.random(-0.5,0.5));
+		}
 	};
+	d.leaf=function(x, y, size, rot)
+	{
+		var leafColor=[d.olor("#24adaf"),d.color("#123456"),d.color("#777777"),d.color("#00ff00"),d.color("#00cc00")];
+		d.fill(d.random(leafColor));
+		d.push();
+		d.translate(x,y);
+		d.rotate(rot);
+		d.arc(0,-size/4-0.5,size,size,d.PI/6,d.PI*5/6,d.CHORD);
+		d.arc(0,size/4+0.5,size,size,-d.PI*5/6,-d.PI/6,d.CHORD);
+		d.pop();
+	}
 };
 new p5(bgSketch,"bg");
 
 /*
 var vmin;
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(700, 400);
 	vmin=min(width,height);
 	noLoop();
 }
 
 function draw() {
   background(255);
+	var size=vmin/4;
+	var delta=vmin/12;
+	var hNo=round(width/40);
+	var vNo=round(height/40);
 	var j;
-	for(j=0;j<13;j++)
+	for(j=0;j<=hNo;j++)
 	{
-		leaf((j+0.5)*width/12,0,100+random(-40,40),PI/2+random(-0.5,0.5));
-		leaf((j+0.5)*width/12,height,100+random(-40,40),PI/2+random(-0.5,0.5));
+		leaf((j+0.5)*width/hNo,0,size+random(-delta,delta),PI/2+random(-0.5,0.5));
+		leaf((j+0.5)*width/hNo,height,size+random(-delta,delta),PI/2+random(-0.5,0.5));
 	}
-	for(j=0;j<13;j++)
+	for(j=0;j<=vNo;j++)
 	{
-		leaf(0,(j+0.5)*height/12,100+random(-40,40),random(-0.5,0.5));
-		leaf(width,(j+0.5)*height/12,100+random(-40,40),random(-0.5,0.5));
+		leaf(0,(j+0.5)*height/vNo,size+random(-delta,delta),random(-0.5,0.5));
+		leaf(width,(j+0.5)*height/vNo,size+random(-delta,delta),random(-0.5,0.5));
 	}
 }
 
